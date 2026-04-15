6f22350ba9bb02058563d77f51fba856fdd89b0e
Emebet <emymak93@yahoo.com>
2026-04-15 17:19:44 +0200

    Split EventList into EventCard component

---
+++ app/src/components/EventList/EventList.jsx
@@ -0,0 +1,16 @@
+import events from "../../data/events.js";
+import EventCard from "./EventCard";
+
+// TODO: split each event below into its own EventCard component
+// TODO: add a "Buy ticket" button to each event card
+// TODO: replace the mock data import with a fetch call to GET /events
+
+export default function EventList() {
+  return (
+    <ul>
+      {events.map((event) => (
+        <EventCard key={event.id} event={event}/>
+      ))}
+    </ul>
+  );
+}
