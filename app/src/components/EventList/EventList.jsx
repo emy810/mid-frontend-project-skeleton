57edcabe6d728ee01a1187a4ce7ac9bb4e536a80
Emebet <emymak93@yahoo.com>
2026-04-17 10:24:39 +0200

    Remove comments

--- app/src/components/EventList/EventList.jsx
+++ app/src/components/EventList/EventList.jsx
@@ -1,8 +1,7 @@
 import events from "../../data/events.js";
 import EventCard from "./EventCard";
 
-// TODO: split each event below into its own EventCard component
-// TODO: add a "Buy ticket" button to each event card
+
 // TODO: replace the mock data import with a fetch call to GET /events
 
 export default function EventList() {
