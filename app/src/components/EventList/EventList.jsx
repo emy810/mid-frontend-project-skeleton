860df9263f3d863c335f178e72eca16bc207853c
Emebet <emymak93@yahoo.com>
2026-04-22 00:23:17 +0200

    Wire EventList with .map() and update HomePage to pass events as props

--- app/src/components/EventList/EventList.jsx
+++ app/src/components/EventList/EventList.jsx
@@ -1,14 +1,13 @@
 import events from "../../data/events.js";
 import EventCard from "./EventCard";
 
-
 // TODO: replace the mock data import with a fetch call to GET /events
 
-export default function EventList() {
+export default function EventList({ event }) {
   return (
     <ul>
       {events.map((event) => (
-        <EventCard key={event.id} event={event}/>
+        <EventCard key={event.id} event={event} />
       ))}
     </ul>
   );
--- app/src/components/HomePage/HomePage.jsx
+++ app/src/components/HomePage/HomePage.jsx
@@ -1,12 +1,8 @@
 // Feel free to replace the content of this component with your own
 
-
+import events from "../../data/events";
 import EventList from "../EventList/EventList";
 
 export default function HomePage() {
-  return <EventList />;
+  return <EventList events={events} />;
 }
-
- 
-
-
